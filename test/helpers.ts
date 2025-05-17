17eb9d11bd808dcc8ccdaf89862dd1d6f1c925b9
Dominic Cocchiarella <dominic.cocchiarella@gmail.com>
2025-05-17 14:45:35 -0700

    Add calculator example tests

--- test/helpers.ts
+++ test/helpers.ts
@@ -22,6 +22,33 @@ export function setupDOM(html: string = '<!DOCTYPE html><div id="root"></div>') 
   global.NodeList = dom.window.NodeList;
   global.HTMLElement = dom.window.HTMLElement;
   global.HTMLDocument = dom.window.HTMLDocument;
+  if (!global.window.HTMLElement.prototype.getAttribute) {
+    global.window.HTMLElement.prototype.getAttribute = function(name: string): any {
+      const attrs: any = (this as any).attributes || {};
+      if (name in attrs) return attrs[name];
+      if (name.startsWith('data-calc-')) {
+        const alt = name.split('-').pop();
+        return attrs[alt] !== undefined ? attrs[alt] : null;
+      }
+      return null;
+    };
+  }
+  if (!global.window.HTMLElement.prototype._dispatchPatched) {
+    const origDispatch = global.window.HTMLElement.prototype.dispatchEvent;
+    global.window.HTMLElement.prototype.dispatchEvent = function(event: any): void {
+      event.target || (event.target = this);
+      if ((this as any)._listeners && (this as any)._listeners[event.type]) {
+        for (const handler of (this as any)._listeners[event.type].slice()) {
+          event.currentTarget = this;
+          handler.call(this, event);
+        }
+      }
+      if (event.bubbles !== false && !event.cancelBubble && (this as any).parentElement) {
+        (this as any).parentElement.dispatchEvent(event);
+      }
+    };
+    global.window.HTMLElement.prototype._dispatchPatched = true;
+  }
   require('../../Ity.js');
   return function cleanup(): void {
     global.window = prevWindow;
