dc3b9b513ed1366eb98875972b0959422d82023c
Dominic Cocchiarella <dominic.cocchiarella@gmail.com>
2025-05-18 21:43:44 -0700

    fix rollup terser plugin dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import { terser } from '@rollup/plugin-terser';
 import dts from 'rollup-plugin-dts';
 
 export default [
